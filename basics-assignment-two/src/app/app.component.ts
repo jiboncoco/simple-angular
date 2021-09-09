df2c3aadbd1f02b57acf064d8cff06aa64f172b7
Art By Code <artbycode.dev@gmail.com>
2021-09-10 01:01:26 +0700

    update basic assignment two

--- basics-assignment-two/src/app/app.component.ts
+++ basics-assignment-two/src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'basics-assignment-two';
+  username = '';
+
+  onReset() {
+    this.username = '';
+  }
 }
