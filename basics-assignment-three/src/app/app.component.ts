46123c3654f1a53931771a13ee7becb3781f6b95
Achmad Fauzi <binar-achmadfauzi@Binars-MacBook-Pro.local>
2021-09-26 14:37:35 +0700

    submit assignment three

--- basics-assignment-three/src/app/app.component.ts
+++ basics-assignment-three/src/app/app.component.ts
@@ -7,10 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   showSecret = false;
-  log : number[] = [];
+  log : any[] = [];
 
   onToggleDetails() {
     this.showSecret = !this.showSecret;
-    this.log.push(this.log.length + 1);
+    // this.log.push(this.log.length + 1);
+    this.log.push(new Date());
   }
 }
