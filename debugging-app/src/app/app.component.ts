308ab9fd550599abb0ea60be590ca0f3739ad98e
Achmad Fauzi <binar-achmadfauzi@Binars-MacBook-Pro.local>
2021-09-29 09:26:55 +0700

    update

--- debugging-app/src/app/app.component.ts
+++ debugging-app/src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'debugging-app';
+    servers: string[] = ['Hello', 'World'];
+
+    onAddServer() {
+        this.servers.push('Another Server');
+    }
+
+    onRemoveServer(id: number) {
+        const position = id + 1;
+        this.servers.splice(position, 1);
+    }
+
 }
